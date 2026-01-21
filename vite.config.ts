aad59709e3fa857c4e385bfbd43a071ee4f5b1b3
Ramy198510 <ramy.amous@gmail.com>
2026-01-21 07:58:19 +0300

    Set base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/rami-amous.github.io/",   // ⬅️ AJOUTE CETTE LIGNE
   server: {
     host: "::",
     port: 8080,
